5a06c52dc81ac5304eecd0352d56f9546c30d2ac
Yaromir Sheydaev <148737776+tyriksheyh4567@users.noreply.github.com>
2025-01-15 16:20:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-install(){
-  if [ -x "$(command -v apt)"];
-  then
-	  echo -e "Installing via apt (Ubuntu)..."
-	  sudo apt install zsh
-	  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
-  elif [ -x "$(command -v apk)"];
-  then
+if [ -x "$(command -v apt)"];
+then
+  echo -e "Installing via apt (Ubuntu)..."
+  sudo apt install zsh
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+elif [ -x "$(command -v apk)"];
+then
   echo -e "Installing via apk (Alpine)..."
   sudo apk add zsh
-  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh}
-install;
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+exit;
