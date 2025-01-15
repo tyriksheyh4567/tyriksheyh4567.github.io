8d239a045ea495977e2f3f07f83b3aada2bf3fd7
Yaromir Sheydaev <148737776+tyriksheyh4567@users.noreply.github.com>
2025-01-15 16:02:16 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+install(){
+  if [ -x "$(command -v apt)"];
+  then
+	  echo -e "Installing via apt (Ubuntu)..."
+	  sudo apt install zsh
+	  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+  elif [ -x "$(command -v apk)"];
+  then
+  echo -e "Installing via apk (Alpine)..."
+  sudo apk add zsh
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+}
+install;
