ee657a8b81be047fb9a7a6ae58920f9bd6554827
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-03-26 14:40:59 +0700

    s38

--- OBHOD/obhod.cmd
+++ OBHOD/obhod.cmd
@@ -1,12 +1,6 @@
 @echo off
 chcp 65001
 cls
-if /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" echo Вы запустили скрипт НЕ через консоль. Запустите через терминал. && pause && exit
-echo Запуск Drover:
-cd drover
-".\drover.exe"
-echo Запуск Tor:
-echo.
-cd ..
+if /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" echo Вы запустили скрипт НЕ через консоль. Запустите через терминал. && pause
 cd tor
 ".\tor.exe" -f ".\torrc"
