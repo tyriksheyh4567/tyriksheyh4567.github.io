2af86bf22cf4a2964caab2ff0dc00652afe804e2
Yaromir Sheydaev <tyrikaltsheyhog@gmail.com>
2025-03-30 21:02:27 +0700

    s27

--- OBHOD/obhod.cmd
+++ OBHOD/obhod.cmd
@@ -2,5 +2,24 @@
 chcp 65001
 cls
 if /i "%comspec% /c %~0 " equ "%cmdcmdline:"=%" echo Вы запустили скрипт НЕ через консоль. Запустите через терминал. && pause
+
+echo Проверка наличия файлов Discord Drover...
+set "drover_found=0"
+for /d %%i in ("%LOCALAPPDATA%\Discord\app-*") do (
+    if exist "%%i\drover.ini" if exist "%%i\version.dll" (
+        echo Найдены файлы Discord Drover в %%i
+        set "drover_found=1"
+    )
+)
+
+if "%drover_found%"=="0" (
+    echo Файлы Discord Drover не найдены.
+    cd drover
+    echo Запуск Tor для скачивания файлов Discord Drover...
+    ".\drover.exe"
+    cd ..
+)
+
+echo Запуск Tor для обхода блокировки Discord...
 cd tor
 ".\tor.exe" -f ".\torrc"
